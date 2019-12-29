820882661ae4320a44e7cf543db16a0eb2411998
Odavinh <penxerscky@gmail.com>
2019-12-29 17:58:03 +0200

    An initial project with default settings was created

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {ApolloProvider} from "@apollo/react-hooks";
+import {Button} from "antd";
+import "antd/dist/antd.css";
+import "./App.scss";
+
+import {client} from "./client";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <Button type="primary">Button</Button>
+      </div>
+    </ApolloProvider>
   );
-}
+};
 
 export default App;
